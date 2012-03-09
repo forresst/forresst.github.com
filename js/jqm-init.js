9efe9c0cf0b1e9ce282120efd035bd3bdcafe33a
forresst <forresst@voila.fr>
2012-03-09 15:10:58 +0100

    Nouveau G.A.

---
+++ js/jqm-init.js
@@ -0,0 +1,34 @@
+$( document ).bind( "mobileinit", function() {
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-28940436-2']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+  // Google analytics
+  $( '[data-role=page]' ).live( 'pageshow', function (event, ui) {
+        try 
+        {
+            if ( location.hash )
+            {
+               url = location.hash;
+            }
+            else 
+            {
+               url = defaulturl;
+            }
+            _gaq.push( ['_trackPageview', url] );
+        } 
+        catch( error ) 
+        {
+	  // Oh no! Call the catch logger
+ 
+      }
+  });
+
+});
+
