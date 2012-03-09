49893e694f5d8522adc1a6e0a9539e71a5ebf8f4
forresst <forresst@voila.fr>
2012-03-09 16:18:04 +0100

    Nouvel essai

--- js/jqm-init.js
+++ js/jqm-init.js
@@ -1,14 +1,5 @@
 $( document ).bind( "mobileinit", function() {
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-28940436-2']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
   // Google analytics
   $( '[data-role=page]' ).live( 'pageshow', function (event, ui) {
         try 
@@ -28,6 +19,15 @@ $( document ).bind( "mobileinit", function() {
 	  // Oh no! Call the catch logger
  
       }
+  
+    var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-28940436-2']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
   });
 
 });
