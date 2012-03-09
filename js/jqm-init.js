91da8c42a67083da7bbb0907f2af79d72f01983b
forresst <forresst@voila.fr>
2012-03-09 17:50:25 +0100

    GA 5

--- js/jqm-init.js
+++ js/jqm-init.js
@@ -1,35 +1,23 @@
-$( document ).bind( "mobileinit", function() {
+$(document).bind("mobileinit", function() {
 
-  // Google analytics
-  $( '[data-role=page]' ).live( 'pageshow', function (event, ui) {
 
-      var _gaq = _gaq || [];
+	$("div").live("pagebeforeshow",function(event,ui) {
+		var header = $("div[data-role='header'] h1",$(this));
+		var title = $.trim(header.text());
+		modtitle = "/" + title + ".html";
+		_gaq.push(["_trackPageview",modtitle]);
+		console.log(modtitle);
+	});
+	
+
+  var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-28940436-2']);
-  
-   try 
-        {
-            if ( location.hash )
-            {
-               url = location.hash;
-            }
-            else 
-            {
-               url = defaulturl;
-            }
-            _gaq.push( ['_trackPageview', url] );
-        } 
-        catch( error ) 
-        {
-	  // Oh no! Call the catch logger
- 
-      }
 
   (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-  });
+
 
 });
-
