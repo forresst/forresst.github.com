b13da1f97d39c5b4fb9fb19d2c603de39346f6a8
forresst <forresst@voila.fr>
2012-03-26 17:15:23 +0200

    Catégories+Google Analytics

--- js/jqm-init.js
+++ js/jqm-init.js
@@ -1,15 +1,21 @@
-$(document).bind("mobileinit", function() {
-  $( '[data-role=page]' ).live( 'pageshow', function (event, ui) {
-    _gaq.push(['_trackPageview']);
-  });
-
   var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-28940436-2']);
+  $(document).ready(function(e) {
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + 
+                  '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+    }); 
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-});
+  $('[data-role=page]').live('pageshow', function (event, ui) {
+    try {
+      _gaq.push(['_setAccount', 'UA-28940436-2']);
+      console.log($.mobile.activePage.attr("data-url"));
+      if ($.mobile.activePage.attr("data-url")) {
+        _gaq.push(['_trackPageview', $.mobile.activePage.attr("data-url")]);
+      } else {
+        _gaq.push(['_trackPageview']);
+      }
+    } catch(err) {}
+  });
